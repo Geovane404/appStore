ccbc38289abcbbf2f9d0ee7dd8b1b0dc0322404d
Geovane Calheira <geovanecalheiro@gmail.com>
2022-02-04 13:26:31 -0400

    Project created

---
+++ src/main/java/com/gtecnologia/store/StoreMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.gtecnologia.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreMySqlApplication.class, args);
+	}
+
+}
