657489a6fc2fa75c7e195a02b7c9f1f536135997
Geovane Calheira <geovanecalheiro@gmail.com>
2022-02-08 10:59:41 -0400

    Product

--- src/main/java/com/gtecnologia/store/config/TestConfig.java
+++ src/main/java/com/gtecnologia/store/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.gtecnologia.store.entities.Category;
 import com.gtecnologia.store.entities.Order;
+import com.gtecnologia.store.entities.Product;
 import com.gtecnologia.store.entities.User;
 import com.gtecnologia.store.enums.OrderStatus;
 import com.gtecnologia.store.repositories.CategoryRepository;
 import com.gtecnologia.store.repositories.OrderRepository;
+import com.gtecnologia.store.repositories.ProductRepository;
 import com.gtecnologia.store.repositories.UserRepository;
 
 @Configuration
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -46,6 +51,13 @@ public class TestConfig implements CommandLineRunner {
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
 	}
 
