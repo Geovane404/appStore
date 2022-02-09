a914fbbbce21f7a5c8ed871a4e183f38d4583b27
Geovane Calheira <geovanecalheiro@gmail.com>
2022-02-09 19:57:24 -0400

    Subtotal e Total methods

--- src/main/java/com/gtecnologia/store/entities/Order.java
+++ src/main/java/com/gtecnologia/store/entities/Order.java
@@ -2,7 +2,9 @@ package com.gtecnologia.store.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -98,6 +100,14 @@ public class Order implements Serializable{
 
 	public void setPayment(Payment payment) {
 		this.payment = payment;
+	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
 	}
 
 	@Override
